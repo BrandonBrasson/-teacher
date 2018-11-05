56cd549b57e2f16a1ab83f2ad108ba3ab74f7a7d
Brandon Brasson <rocksong622@gmail.com>
2018-11-05 20:26:18 +0000

    Add existing file

--- teachers/bin/teaher.rb
+++ teachers/bin/teaher.rb
@@ -1,57 +1,41 @@
-class Teach
-  attr_accessor name: height: Ian: Tracy: JJ: Antoin:
-#teachers = [{name: 'Ian', height: 76}, {name: 'Tracy', height: 62}, {name: 'JJ', height: 70}, {name: 'Antoin', height: 71} ]
-  @@all
-
-def initalize(teachers)
-  binding.pry
-   @@all
- end
-
+teachers = [{name: 'Ian', height: 76}, {name: 'Tracy', height: 62}, {name: 'JJ', height: 70}, {name: 'Antoin', height: 71} ]
 
 def names(teachers)
   # returns an array of all of the teachers names ['Ian','Tracy','JJ','Antoin']
-  teachers.collect do |teacher|
-    return ['Ian','Tracy','JJ','Antoin']
-#iterate
-    # pull out the name value
+  teachers.collect do |teacher| #iterate
+  puts ['Ian','Tracy','JJ','Antoin']
   end
+end
 
-
-#def name (){
-#  collect.each do |teacher|
-#  return ['Ian','Tracy','JJ','Antoin' ]  #iterate
-#}
-#def name (teachers,height){
- # collect.each do |teacher|
-  #return ['height: 76 height: 62 height: 70 height: 71' ]  #iterate
-#}
-#def name (teachers,height){
-  #collect.each do |teacher|
-  #return [''Ian','Tracy','JJ','Antoin']  #iterate
-
+1. iterate over the collection
+2. pull out the name value
+2.5. 'return' the array
 
 
 #puts (1..100).collect { |i| (fb = [["Fizz"][i % 3], ["Buzz"][i % 5]].compact.join).empty? ? i : fb }
 
 
 def starts_with_j(teachers)
- teachers.collect do |teacher|
-  return teachers ('height: 76 height: 62 height: 70 height: 71')
+  # returns an array of all of the teachres whose name starts with J
+  teachers.collect do |teacher| #iterate
+  if true
+  puts (teacher.collect { |teacher| (jj = [["Ian"], ["Tracy"], ["Antoin"].compact.join).empty? }
+else
+  puts (teacher.collect { |teacher| (jj = ['Ian','Tracy','JJ','Antoin'].compact.join).empty? }
 end
 
-
 def tall_teachers(teachers)
-  teachers.collect do |teacher|
-    return teachers ('height: 76 height: 62 height: 70 height: 71')
+  teachers.collect do |teacher| #iterate
+  puts [{name: 'Ian', height: 76}, {name: 'Tracy', height: 62}, {name: 'JJ', height: 70}, {name: 'Antoin', height: 71} ]
 end
 
 def any_start_with_t?(teachers)
- teachers.collect do |teacher|
-   return  ['Ian','','JJ','Antoin']
   # returns true if any of the teachers have a name starting with the letter 'T', otherwise returns false
+  if true
+    puts (teacher.collect { |teacher| (jj = ['Ian','JJ','Antoin'].compact.join).empty? }
+  else
+    puts teacher.collect { |teacher| (jj = ['Ian','Tracy','JJ','Antoin'].compact.join).empty? }
+  end
 end
 
-
 end
- 
