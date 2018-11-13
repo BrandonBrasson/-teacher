c09b43c3dc989a8692105ac358f1eae04a6b2789
Brandon Brasson <31114139+BrandonBrasson@users.noreply.github.com>
2018-11-13 16:01:08 -0500

    Update teaher.rb

--- teachers/bin/teaher.rb
+++ teachers/bin/teaher.rb
@@ -1,41 +1,41 @@
-teachers = [{name: 'Ian', height: 76}, {name: 'Tracy', height: 62}, {name: 'JJ', height: 70}, {name: 'Antoin', height: 71} ]
 
 def names(teachers)
   # returns an array of all of the teachers names ['Ian','Tracy','JJ','Antoin']
   teachers.collect do |teacher| #iterate
-  puts ['Ian','Tracy','JJ','Antoin']
-  end
+  return ['Ian','Tracy','JJ','Antoin']
 end
-
-1. iterate over the collection
-2. pull out the name value
-2.5. 'return' the array
-
-
-#puts (1..100).collect { |i| (fb = [["Fizz"][i % 3], ["Buzz"][i % 5]].compact.join).empty? ? i : fb }
 
 
 def starts_with_j(teachers)
   # returns an array of all of the teachres whose name starts with J
   teachers.collect do |teacher| #iterate
-  if true
-  puts (teacher.collect { |teacher| (jj = [["Ian"], ["Tracy"], ["Antoin"].compact.join).empty? }
-else
-  puts (teacher.collect { |teacher| (jj = ['Ian','Tracy','JJ','Antoin'].compact.join).empty? }
-end
+    for teachers in teachers:
+    if teachers[0]=='JJ':
+    teachers.collect(teachers)
+    return teachers
+end 
 
-def tall_teachers(teachers)
-  teachers.collect do |teacher| #iterate
-  puts [{name: 'Ian', height: 76}, {name: 'Tracy', height: 62}, {name: 'JJ', height: 70}, {name: 'Antoin', height: 71} ]
+def tall_teachers(teachers):
+    teachers.collect do |teacher|
+ return [{name: 'Ian', height: 76}, {name: 'Tracy', height: 62}, {name: 'JJ', height: 70}, {name: 'Antoin', height: 71} ]
 end
 
 def any_start_with_t?(teachers)
   # returns true if any of the teachers have a name starting with the letter 'T', otherwise returns false
-  if true
-    puts (teacher.collect { |teacher| (jj = ['Ian','JJ','Antoin'].compact.join).empty? }
-  else
-    puts teacher.collect { |teacher| (jj = ['Ian','Tracy','JJ','Antoin'].compact.join).empty? }
-  end
+    teachers.collect do |teacher|
+  for teacher in teachers:
+  if teacher[0]=='T':
+  return True
+  return False
 end
 
-end
+
+def teachers():
+teachers = names()
+print("We have ",teachers)
+any_start_with_t? = any_start_with_t?)
+print(" ian",any_start_with_t?)
+print("teachers")
+print(tall_teachers())
+print("Do we have any teacher that starts_with_j: ",starts_with_j(teachers)
+ end
