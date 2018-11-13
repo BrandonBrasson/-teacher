7fd4fd70f93ea5f0909c33fe8fb78c3c79737153
Brandon Brasson <rocksong622@gmail.com>
2018-11-13 22:49:51 +0000

    Add existing file

--- teachers/bin/teaher.rb
+++ teachers/bin/teaher.rb
@@ -1,3 +1,4 @@
+teachers = [{name: 'Ian', height: 76}, {name: 'Tracy', height: 62}, {name: 'JJ', height: 70}, {name: 'Antoin', height: 71} ]
 
 def names(teachers)
   # returns an array of all of the teachers names ['Ian','Tracy','JJ','Antoin']
@@ -6,14 +7,14 @@ def names(teachers)
 end
 
 
-def starts_with_j(teachers)
+def starts_with_j(teachers):
   # returns an array of all of the teachres whose name starts with J
   teachers.collect do |teacher| #iterate
     for teachers in teachers:
-    if teachers[0]=='JJ':
+     if teachers[0]=='J':
     teachers.collect(teachers)
-    return teachers
-end 
+    return teacher
+end
 
 def tall_teachers(teachers):
     teachers.collect do |teacher|
@@ -24,7 +25,7 @@ def any_start_with_t?(teachers)
   # returns true if any of the teachers have a name starting with the letter 'T', otherwise returns false
     teachers.collect do |teacher|
   for teacher in teachers:
-  if teacher[0]=='T':
+   if teacher[0]=='T':
   return True
   return False
 end
@@ -37,5 +38,7 @@ any_start_with_t? = any_start_with_t?)
 print(" ian",any_start_with_t?)
 print("teachers")
 print(tall_teachers())
-print("Do we have any teacher that starts_with_j: ",starts_with_j(teachers)
- end
+print("Do we have any teacher that starts_with_j: ",starts_with_j(teachers))
+
+
+teacher()
